c8842071a008d2a6c7b7c4db6f79222a15dd0c34
Nyck <87528900+LoneNyck@users.noreply.github.com>
2025-01-29 14:57:13 +0100

    Add files via upload

---
+++ get_values.py
@@ -0,0 +1,21 @@
+from PIL import Image as im
+
+class Pixel():
+    def __init__(self, span: tuple, sym):
+        self.span = span
+        self.sym = sym
+
+image = im.open("giovi.jpeg").convert("L")
+
+width, height = image._size
+
+pixels = list(image.getdata())
+
+pixel_matrix = [[pixels[width * i + j] for j in range(width)] for i in range(height)]
+
+with open("values.txt", "w") as f:
+    f.write(f"{width}, {height}\n")
+    for line in pixel_matrix:
+        for pixel in line:
+            f.write(f"{str(pixel)} ")
+        f.write("\n")
---
+++ imageCreator.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    FILE *fIn, *fOut;
+    int width, height;
+    int i, j;
+    int val;
+
+    if(argc != 3){
+        cout << "Argomenti insufficienti in chiamata di programma" << endl;
+        return -1;
+    }
+
+    fIn = fopen(argv[1], "r");
+    fOut = fopen(argv[2], "w");
+
+    if(fIn == NULL || fOut == NULL){
+        cout << "Errore nell'apertura del file" << endl;
+        return -1;
+    }
+
+    fscanf(fIn, "%d, %d", &width, &height);
+
+    i = 0;
+    while(i < height && !feof(fIn)){
+        j = 0;
+        while(j < width && !feof(fIn)){
+            fscanf(fIn, "%d", &val);
+            switch(val){
+                case 0 ... 25: 
+                    fprintf(fOut, "@ ");
+                    break;
+                case 26 ... 50: 
+                    fprintf(fOut, "# ");
+                    break;
+                case 51 ... 75: 
+                    fprintf(fOut, "%% ");
+                    break;
+                case 76 ... 100:
+                    fprintf(fOut, "* ");
+                    break;
+                case 101 ... 125: 
+                    fprintf(fOut, "+ ");
+                    break;
+                case 126 ... 150: 
+                    fprintf(fOut, "= ");
+                    break;
+                case 151 ... 175: 
+                    fprintf(fOut, "- ");
+                    break;
+                case 176 ... 200: 
+                    fprintf(fOut, ": ");
+                    break;
+                case 201 ... 225: 
+                    fprintf(fOut, ". ");
+                    break;
+                case 226 ... 255: 
+                    fprintf(fOut, "  ");
+                    break;
+            }
+            j++;
+        }
+        fprintf(fOut, "\n");
+        i++;
+    }
+
+    fclose(fIn);
+    fclose(fOut);
+
+    return 0;
+}
+
