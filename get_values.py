154f00695c99e7bbb5ad6cf9ca60fd0694a91ead
Nyck <87528900+LoneNyck@users.noreply.github.com>
2025-01-29 15:04:09 +0100

    Update get_values.py

--- get_values.py
+++ get_values.py
@@ -5,7 +5,7 @@ class Pixel():
         self.span = span
         self.sym = sym
 
-image = im.open("giovi.jpeg").convert("L")
+image = im.open("my_image.png").convert("L")
 
 width, height = image._size
 
@@ -13,7 +13,7 @@ pixels = list(image.getdata())
 
 pixel_matrix = [[pixels[width * i + j] for j in range(width)] for i in range(height)]
 
-with open("values.txt", "w") as f:
+with open("myFile.txt", "w") as f:
     f.write(f"{width}, {height}\n")
     for line in pixel_matrix:
         for pixel in line:
